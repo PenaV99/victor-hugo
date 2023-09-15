077c867e8ada64b75292cb0dc696af5e0ac9b30d
webinit-io <54963813+PenaV99@users.noreply.github.com>
2023-09-15 11:40:53 -0500

    "Change Config"

---
+++ AgnusDei_files/dgT.js
@@ -0,0 +1,8 @@
+// Qualaroo for metmuseum.org
+// (C) 2023 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 54327, generated: 2023-06-12 17:17:09 UTC
+//$ client: 2.0.64
+
+//Your Qualaroo account is suspended or disabled.  Please see your dashboard at https://app.qualaroo.com (or email support@qualaroo.com).
---
+++ AgnusDei_files/web_surveys.js
@@ -0,0 +1,75 @@
+var _sTrackingAlreadyPresent = (typeof window._svd !== 'undefined' && typeof window._svc !== 'undefined');var _svc = window._svc || {};var _svd = window._svd || {};_svc.workspaceKey = _svc.workspaceKey || '5c82727293475c10de9d0ffa0ace00b3';_svd.flags = _svd.flags || {"async_consumers":true};_svd.surveys = _svd.surveys || [{"id":"4e5bfee0aee483b8","activated":true,"name":"User Satisfaction (Plan Your Visit)","type":"WidgetSurvey","points":[{"settings":{"logic":[{"order_number":0,"uid":356341257053,"go_to":2158531,"condition":"is","operator":"or","value":[5849832,5849833]}],"text_on_the_left":"Very Poor","text_on_the_right":"Excellent"},"id":2158529,"type":"SurveyQuestion","answer_type":"smiley_scale","content":"<b>How are you finding your experience on this page?</b>","description":"","max_path":4,"answers":[{"id":5849829,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Extremely unsatisfied"},{"id":5849830,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Unsatisfied"},{"id":5849831,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Neutral"},{"id":5849832,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Happy"},{"id":5849833,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Extremely happy"}]},{"settings":{"logic":[],"randomize_answers":true,"randomize_except_last":false,"next_survey_point_id":null,"choice":{"min":1,"max":0,"text":""},"mandatory_comment":true},"id":2158653,"type":"SurveyQuestion","answer_type":"multiple","content":"<b>What information on this page do you find most useful?</b>","description":"","max_path":3,"answers":[{"id":5849983,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Accessibility Info","add_comment":false},{"id":5849984,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Exhibitions On View","add_comment":false},{"id":5849986,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Guides (Audio, Digital, Highlights Tour)","add_comment":false},{"id":5849988,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Membership (Signup, Info)","add_comment":false},{"id":5849989,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Museum Info (Hours, Location, Directions)","add_comment":false},{"id":5849990,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Purchase a Ticket","add_comment":false},{"id":5849991,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Special Announcements","add_comment":false},{"id":5849992,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Visiting Guidelines","add_comment":false}]},{"settings":{"logic":[],"randomize_answers":true,"randomize_except_last":false,"next_survey_point_id":null,"choice":{"min":1,"max":0,"text":""},"mandatory_comment":true},"id":2158760,"type":"SurveyQuestion","answer_type":"multiple","content":"<b>What information on this page do you find least useful?</b>","description":"","max_path":2,"answers":[{"id":5850129,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Accessibility Info","add_comment":false},{"id":5850130,"type":null,"user_tag":"","next_survey_point_id":null,"possible_answer":"Exhibitions On View","add_comment":false},{"id":5850131,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Guides (Audio, Digital, Highlights Tour)","add_comment":false},{"id":5850132,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Membership (Signup, Info)","add_comment":false},{"id":5850133,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Museum Info (Hours, Location, Directions)","add_comment":false},{"id":5850134,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Purchase a Ticket","add_comment":false},{"id":5850135,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Special Announcements","add_comment":false},{"id":5850136,"type":null,"user_tag":"","next_survey_point_id":-1,"possible_answer":"Visiting Guidelines","add_comment":false}]},{"settings":{"logic":[],"mandatory":true,"user_tag":"","next_survey_point_id":null},"id":2161665,"type":"SurveyQuestion","answer_type":"text","content":"<b>What would you like more information about on this page?</b>","description":"","max_path":1,"answers":[{"id":5855321,"type":null,"user_tag":null,"next_survey_point_id":null}]},{"settings":{"user_tag":"","link":"","facebook_link":"","linkedin_link":"","twitter_link":"","timeout_display_count":5,"open_new_card":true,"text":"","next_survey_point_id":-1},"id":2158531,"type":"SurveyCta","answer_type":"empty","content":"Thank you for your feedback!","description":"","max_path":0}],"project_id":852088,"integrations":{},"links":[{"link":"https://www.metmuseum.org/visit/plan-your-visit","visible":true,"link_type":"exact","regexp_text_pattern":"^metmuseum\\.org/visit/plan\\-your\\-visit\\/?$"}],"audiences_ids":[974690],"provider":null,"theme_id":342545,"global_tags":null,"global_close_tags":null,"settings":{"has_responses":true,"appear_method":"delayed","close_icon":"bottom","close_on_urlchange":false,"survey_language":"","display_all":false,"display_delay":10,"display_not_engaged":false,"display_per":null,"display_percentage":100,"display_scroll":80,"launch":{"start_at":null,"end_at":null,"responses_limit":2000},"messages":{"submit_text":"Submit","tooltip":"Please answer our short survey","answer_required_text":"Answer required","dropdown_placeholder":"Type or select an option","thankyou_text":"Thank you for taking part in our survey"},"overlay":null,"place_to_show":"right","recurring":false,"recurring_period":86400,"run_minimalized":false,"run_minimalized_mobile":false,"show_close":false,"show_minimize":true,"show_progress_bar":false,"hide_footer":true,"languages":null}}];_svd.themes = _svd.themes || [{"settings":{"company_logo":"","personalization":false,"short_text_message":"","avatar_image":""},"id":342545,"color_scheme":{"question":"#674747","answer":"#674747","button":"#f96666","background":"#eeeeee","progress_bar":"#f96666"},"type":"MicroTheme"}];_svd.permissions = _svd.permissions || {"targeting_javascript_api":true};_svd.audiences = _svd.audiences || [{"id":974690,"filters":[]}];_svd.integrations = _svd.integrations || [{"enabled":true,"provider":"google_analytics","enabled_at":1690997851,"enabled_by_user_id":1048278}];_svd.installing = _svd.installing || false;_svd.translations = _svd.translations || null;// Generated at: 2023\u002D09\u002D14\u002020\u003A33\u003A44
+(function () {
+  if (_sTrackingAlreadyPresent) {
+    return;
+  }
+
+  if (document && document.head) {
+    var linkElement = document.createElement('link');
+    linkElement.href = 'https\u003A\/\/surveys\u002Dstatic.survicate.com/fonts/fonts.css';
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    document.head.appendChild(linkElement);
+  }
+
+  var isIE = window.navigator.userAgent.indexOf('MSIE') !== -1 || window.navigator.userAgent.match(/Trident.*rv\:11\./);
+  var isSafari = window.navigator.vendor && window.navigator.vendor.indexOf('Apple') > -1 && window.navigator.userAgent && window.navigator.userAgent.indexOf('CriOS') == -1 && window.navigator.userAgent.indexOf('FxiOS') == -1;
+
+  var isOldSafari = false;
+  if (isSafari && window.navigator.userAgent) {
+    var match = window.navigator.userAgent.match(/Version.([0-9]+)\./);
+    if (match && match.length === 2) {
+      if (parseInt(match[1]) < 11) {
+        isOldSafari = true;
+      }
+    }
+  }
+  var isOldEdge = window.navigator.userAgent.match(/Edge\/(15\.15|18\.18)/);
+
+  var coreUrls = ['https://surveys-static.survicate.com/widget_core-20.2.2.js'];
+  var e = document.getElementsByTagName('script')[0];
+
+  if (isIE || isOldSafari || isOldEdge) {
+    var css = 'h1 { width: 75%; height: 50%; overflow: auto; margin: auto; position: absolute; top: 0; left: 0; bottom: 0; right: 0; text-align: center; font-family: sans-serif; }'
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    var message = 'Your browser doesn’t meet our security standards. Please update your browser to answer this survey.'
+    var body = document.querySelector('body');
+    var splash = document.createElement('div');
+    var survey = document.querySelector('.survicate-survey');
+
+    if(!head || !survey) {
+      return;
+    }
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+
+    if (style.styleSheet){
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    splash.classList.add('splash-screen');
+    splash.innerHTML = '<h1>' + message + '</h1>';
+
+    if(!body) {
+      return;
+    }
+
+    body.appendChild(splash);
+
+    return;
+  }
+
+  for (var i = 0; i < coreUrls.length; i++) {
+    var s = document.createElement('script');
+    s.setAttribute('crossorigin', 'anonymous');
+    s.src = coreUrls[i];
+    s.async = true;
+    e.parentNode.insertBefore(s, e);
+  }
+})();
